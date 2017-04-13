3c6f712ccbb8ba5542792bd6ea8cbc8f150cfb13
Babibubebon <babibubebon@babibubebo.org>
2017-04-13 17:47:55 +0900

    Change domain name from mediaarts-db.jp to mediaarts-db.bunka.go.jp

--- lib/Constants.php
+++ lib/Constants.php
@@ -1,4 +1,4 @@
 <?php
-const URI_BASE = 'http://mediaarts-db.jp';
+const URI_BASE = 'https://mediaarts-db.bunka.go.jp';
 const URI_ANIME_SERIES = URI_BASE . '/an/anime_series';
 const URI_ANIME_LIST   = URI_BASE . '/an/anime_series?asf%5Bkeyword%5D=*&asf%5Bper%5D=1000';
